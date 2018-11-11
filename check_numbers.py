6dba7e2aa8155b289e91f400a50ff6dd1589dad5
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-11-11 07:00:09 -0500

    testing some conditionals

---
+++ check_numbers.py
@@ -0,0 +1,8 @@
+user_input = float(input("Please input a number: "))
+
+if user_input < 100:
+    print("the number is Smaller")
+elif user_input ==100:
+    print("The number is the same")
+else:
+    print ("the nunber is Greater")
