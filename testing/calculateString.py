b197e6f1f85f3f112a793ede584d5c4740854979
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-11-11 18:49:39 -0500

    playing with the datatypes

--- testing/calculateString.py
+++ testing/calculateString.py
@@ -2,10 +2,16 @@ def calcString(f):
     if type(f) == str:
         return len(f)
 
-    else:
+    elif type(f) == int:
         return "Sorry integers don't have legth."
+
+    elif type(f) == float:
+        return "Sorry floats don't have legth. "
+
+    else:
+        return "the input don't have length"
 
 
 
 #f = input("please input the string to calculate: ")
-print(calcString("10"))
+print(calcString(True))
