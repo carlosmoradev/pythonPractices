7a299c1823b39e3cd8b667b2abc63d21669283f3
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-11-11 20:03:18 -0500

    additional test with loops

---
+++ testing/loop2.py
@@ -0,0 +1,15 @@
+mylist = [1, 2, 3, 4, 5]
+#expected output: 3, 4, 5
+for i in mylist[2:]:
+    print(i)
+
+print("Second option ")
+
+#second option
+
+print(mylist)
+
+for s in mylist:
+    if s > 2:
+        print(s)
+
